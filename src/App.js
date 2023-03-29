425fc01a4e06bbc2aa224c3b3ca8807eeb8a75b0
Vedansh Dubey <dubeyvs_1@rknec.edu>
2023-03-29 23:06:41 +0530

    homepage changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Navbar from './Sections/Navbar';
 import {HashRouter, Route, Routes, Redirect, Navigate } from 'react-router-dom';
 import Events from './Pages/events';
 import { Technohunt, YouthParliament } from './Pages/event_info';
-// import Homepage from './Pages/homepage';
+import Homepage from './Pages/homepage';
 
 import Loader from './Sections/loader';
 
 
-const LazyHomepage = lazy(() => import('./Pages/homepage'));
+// const LazyHomepage = lazy(() => import('./Pages/homepage'));
 const LazyEvents = lazy(() => import('./Pages/events'));
 const LazyAbout = lazy(() => import('./Pages/About'));
 const LazyGalleryPage = lazy(() => import('./Pages/Gallery'));
@@ -22,7 +22,7 @@ function App() {
       <Suspense fallback={<Loader />}>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<LazyHomepage />} />
+          <Route exact path="/" element={<Homepage />} />
           <Route path="/events" element={<LazyEvents />} />
           <Route path="/events/1" element={<Technohunt />} />
           <Route path="/events/2" element={<YouthParliament />} />
