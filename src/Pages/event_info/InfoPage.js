590e1aa9d908d4610eba1487ddb7b208b0c9b023
Vedansh Dubey <dubeyvs_1@rknec.edu>
2023-04-03 22:52:47 +0530

    Added events information

--- src/Pages/event_info/InfoPage.js
+++ src/Pages/event_info/InfoPage.js
@@ -7,9 +7,17 @@ export const YouthParliament = () => {
     <div className='info'>
       <EventPage
         event_name="Youth Parliament"
-        description=" "
-        rules=" "
-        registration=" " />
+        description="Join our youth parliament to make a difference in your community! We seek passionate and engaged youth to debate societal issues, enhance public speaking skills, and connect with like-minded peers.
+         Sign up now to have your voice heard and make an impact."
+        rules="You are provided with 4 bills.
+               You will be divided in 2 parties.
+               Ruling Party speaks for the motion.
+               Opposition Party speaks against the motion.
+               Every party member has 2-3 mins to put his/her point in front of the chair and entire parliament.
+               If any party member wants to give rebuttal, he can ask chair and if he allows, he can speak.
+               Pannel descission would be considered final."
+        registration= "<a href='https://rcoem.in/anon_evForm.htm?ev=143'>Ahoyy! Click Here</a> <br>Event Incharge<br>Aditi Tiwari<br>Bharat Shukla<br><br>For more details, contact <br> Bharat Shukla - 9021675390"
+        />
     </div>
   );
 };
@@ -18,10 +26,15 @@ export const Technohunt = () => {
   return (
     <div className='info'>
       <EventPage
-        event_name="Techno Hunt"
-        description=" "
-        rules=" "
-        registration=" " />
+        event_name="Technohunt"
+        description="Ahoy mates! Ready to set sail on a thrilling tech adventure? Join us for TechnoHunt, the ultimate pirate-themed challenge. 
+                      With pirate-themed challenges and hints from the coordinator, this event is perfect for tech-savvy pirates. 
+                      Don't miss out on the treasure - sign up now for TechnoHunt! "
+        rules="Team participation (4 members)
+               Entry fee: Rs. 200 /-
+               Date-20-4-2023
+               3 Rounds"
+        registration="<a href='https://rcoem.in/anon_evForm.htm?ev=143'>Ahoyy! Click Here</a> <br>Event Incharge<br>Himanshu Shahu<br>Ritesh Yadav<br><br>For more details, contact <br> Himanshu Shahu -  9370299452<br>Ritesh Yadav - 7972907036" />
     </div>
   );
 };
@@ -31,9 +44,13 @@ export const BoxCricket = () => {
     <div className='info'>
       <EventPage
         event_name="Box Cricket"
-        description=" "
-        rules=" "
-        registration=" " />
+        description="Box cricket is a fast-paced game that challenges players' reflexes, coordination, and strategic thinking. It's perfect for players of all skill levels, offering a unique and exciting way to enjoy the sport. Gear up and grab your team for an unforgettable box cricket showdown today!"
+
+        rules="6 members per team, open to girls or boys.
+               IT department building and start of IT parking will serve as boundaries.
+               The basketball court will be the playing area.  
+               Umpire decision will be last decision."
+        registration="<a href='https://rcoem.in/anon_evForm.htm?ev=143'>Ahoyy! Click Here</a> <br>Event Incharge<br>Atharva Kharabe<br>Yash Ghoderao<br><br>For more details, contact <br> Atharva Kharabe - 9579319410<br>Yash Ghoderao - 9422213962 " />
     </div>
   );
 };
@@ -43,9 +60,11 @@ export const Valorant = () => {
     <div className='info'>
       <EventPage
         event_name="Valorant"
-        description=" "
-        rules=" "
-        registration=" " />
+        description="Compete in our Valorant tournament and prove your skills in this intense game. With exciting prizes and the chance to face the best, this is a competition you can't miss. Sign up now and let the games begin!"
+        rules="Teams consist of 5 players.
+               The first team to win 13 rounds wins the match.
+               Each player starts with a set amount of credits to purchase weapons and abilities."
+        registration="<a href='https://rcoem.in/anon_evForm.htm?ev=143'>Ahoyy! Click Here</a> <br>Event Incharge<br>Sanskar Wankhede<br>Aryan Mishra<br><br>For more details, contact <br>Sanskar Wankhede - 9322173579<br>Aryan Mishra - 7249668272 " />
     </div>
   );
 };
@@ -55,9 +74,12 @@ export const Recrewtment = () => {
     <div className='info'>
       <EventPage
         event_name="Recrewtment"
-        description=" "
-        rules=" "
-        registration=" " />
+        description=" Are you ready to experience the thrill of the recruitment process? Look no further than Recrewtment! This exciting event is designed to give students a taste of what it takes to stand out in the job market."
+        rules="Individual participation
+               Entry fee: Rs. 100 /-
+               Seminar on placements preparation
+               Rounds: Aptitude,Debugging, Coding and HR round "
+        registration="<a href='https://rcoem.in/anon_evForm.htm?ev=143'>Ahoyy! Click Here</a> <br>Event Incharge<br>Mayank Pandey<br>Anand Upadhyay<br><br>For more details, contact <br>Mayank Pandey - 96532799879<br>Anand Upadhyaya - 9076743679 " />
     </div>
   );
 };
@@ -68,9 +90,12 @@ export const CascadingCaptain = () => {
     <div className='info'>
       <EventPage
         event_name="Cascading Captain"
-        description=" "
-        rules=" "
-        registration=" " />
+        description="Are you ready to showcase your front-end development skills? Join us for an exciting event where you'll have the opportunity to design a static web page based on a given set of keywords within a limited timeframe."
+        rules="  Individual participation
+                 Entry fee: Rs. 100 /- 
+                 Platform: Online
+                 Participants will be given a set of keywords where they have to design static web page"
+        registration="<a href='https://rcoem.in/anon_evForm.htm?ev=143'>Ahoyy! Click Here</a> <br>Event Incharge<br>Niraj Deshpande<br>Akshit Bobde<br><br>For more details, contact <br> Niraj Deshpande -  9322847539<br>Akshit Bobde - 9850324230 " />
     </div>
   );
 };
@@ -81,9 +106,9 @@ export const AwsWorkshop = () => {
     <div className='info'>
       <EventPage
         event_name="AWS Workshop"
-        description=" "
-        rules=" "
-        registration=" " />
+        description="A hands on AWS workshop which will include basics of AWS and how to use various AWS services to build and run internet applications. You will gain valuable skills to create reliable, scalable, and affordable solutions using AWS."
+        rules="Hurrray! There are no rules"
+        registration="<a href='https://rcoem.in/anon_evForm.htm?ev=143'>Ahoyy! Click Here</a> <br>Event Incharge<br>Chinmayee Sonboir<br>Hardik Pandey<br><br>For more details, contact <br>Hardik Pandey - 9028408407 " />
     </div>
   );
 };
@@ -92,11 +117,15 @@ export const AwsWorkshop = () => {
 export const InnovationIsland = () => {
   return (
     <div className='info'>
-      <EventPage
-        event_name="Innovation Island"
-        description=" "
-        rules=" "
-        registration=" " />
+<EventPage
+    event_name="Innovation Island"
+    description="Innovation Island is an entrepreneurship event that requires participants to pitch their innovative idea. This event provides a platform for aspiring entrepreneurs to showcase their innovative ideas, receive feedback from experts, and win exciting prizes."
+    rules="Number of team members: 1 - 3 members
+           Entry for Event: Rs. 200/-
+           Rounds: Online submission of abstract in first round and Offline presentation in second round"
+    registration="<a href='https://rcoem.in/anon_evForm.htm?ev=143'>Ahoyy! Click Here</a> <br>Event Incharge<br>Atharva Baheti<br>Rishi Ojha<br>Divya Dhage<br><br>For more details, contact <br>Atharva Baheti - 7517898918<br>Rishi Ojha - 9764061088 " 
+/>
+
     </div>
   );
 };
